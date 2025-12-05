349c554d9d86f63245a680f49e06fd65b1afbb5e
Michael Graves <29133471+michaelgrav@users.noreply.github.com>
2025-12-04 22:08:33 -0500

    Fix linting errors

--- src/components/Welcome/Welcome.tsx
+++ src/components/Welcome/Welcome.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useRef, useState } from 'react';
 import axios from 'axios';
-import { AppShell, Burger, Center, Loader, Skeleton, Table, Title, Text, Group, Avatar } from '@mantine/core';
+import { AppShell, Burger, Center, Group, Loader, Table, Text, Title } from '@mantine/core';
 import { useDisclosure, useMediaQuery } from '@mantine/hooks';
 
 export function Welcome() {
@@ -65,7 +65,9 @@ export function Welcome() {
   const dailyRows = weatherForecast?.properties?.periods.map((period: any) => (
     <Table.Tr key={period.name}>
       <Table.Td>{period.name}</Table.Td>
-      <Table.Td style={{ textAlign: 'right' }}>{period.temperature + period.temperatureUnit}</Table.Td>
+      <Table.Td
+        style={{ textAlign: 'right' }}
+      >{`${period.temperature}${period.temperatureUnit}`}</Table.Td>
     </Table.Tr>
   ));
 
@@ -84,8 +86,14 @@ export function Welcome() {
   const hourlyRows = hourlyWeatherForecast?.properties?.periods.map((period: any) => (
     <Table.Tr key={period.startTime}>
       <Table.Td>{formatHour(period.startTime)}</Table.Td>
-      <Table.Td style={{ textAlign: 'right' }}>{period.probabilityOfPrecipitation?.value != null ? period.probabilityOfPrecipitation.value + '%' : ''}</Table.Td>
-      <Table.Td style={{ textAlign: 'right' }}>{period.temperature != null ? period.temperature + period.temperatureUnit : ''}</Table.Td>
+      <Table.Td style={{ textAlign: 'right' }}>
+        {period.probabilityOfPrecipitation?.value != null
+          ? `${period.probabilityOfPrecipitation.value}%`
+          : ''}
+      </Table.Td>
+      <Table.Td style={{ textAlign: 'right' }}>
+        {period.temperature != null ? `${period.temperature}${period.temperatureUnit}` : ''}
+      </Table.Td>
     </Table.Tr>
   ));
 
@@ -100,10 +108,21 @@ export function Welcome() {
       }}
     >
       <AppShell.Header>
-        <Group style={{ width: '100%', display: 'flex', justifyContent: 'space-between', alignItems: 'center', height: '100%' }}>
+        <Group
+          style={{
+            width: '100%',
+            display: 'flex',
+            justifyContent: 'space-between',
+            alignItems: 'center',
+            height: '100%',
+          }}
+        >
           <Group style={{ display: 'flex', alignItems: 'center', gap: 8 }}>
             <Burger opened={opened} onClick={toggle} hiddenFrom="sm" size="sm" />
-            <Text size={isSmall ? 'md' : 'xl'} ml={isSmall ? 0 : 10}> ☀️ Sun Clouds ☁️</Text>
+            <Text size={isSmall ? 'md' : 'xl'} ml={isSmall ? 0 : 10}>
+              {' '}
+              ☀️ Sun Clouds ☁️
+            </Text>
           </Group>
 
           <Text
@@ -126,27 +145,26 @@ export function Welcome() {
       </AppShell.Header>
 
       <AppShell.Navbar>
-
         <Title order={5} ta="center" mt={25} mb={15}>
           7-Day Forecast
         </Title>
 
         {/* return loading circle conditionally if the API call hasnt returned yet */}
-        { dailyRows ?
-        <Table striped highlightOnHover>
-          <Table.Thead>
-            <Table.Tr>
-              <Table.Th>Day</Table.Th>
-              <Table.Th style={{ textAlign: 'right' }}>Temperature</Table.Th>
-            </Table.Tr>
-          </Table.Thead>
-          <Table.Tbody>{dailyRows}</Table.Tbody>
-        </Table> :
-
-        <Center>
-          <Loader color="yellow" mt={20}/>
-        </Center>
-        }
+        {dailyRows ? (
+          <Table striped highlightOnHover>
+            <Table.Thead>
+              <Table.Tr>
+                <Table.Th>Day</Table.Th>
+                <Table.Th style={{ textAlign: 'right' }}>Temperature</Table.Th>
+              </Table.Tr>
+            </Table.Thead>
+            <Table.Tbody>{dailyRows}</Table.Tbody>
+          </Table>
+        ) : (
+          <Center>
+            <Loader color="yellow" mt={20} />
+          </Center>
+        )}
       </AppShell.Navbar>
 
       <AppShell.Main>
@@ -155,24 +173,22 @@ export function Welcome() {
         </Title>
 
         {/* return loading circle conditionally if the API call hasnt returned yet */}
-        { hourlyRows ? 
-
-        <Table striped highlightOnHover>
-          <Table.Thead>
-            <Table.Tr>
-              <Table.Th>Time</Table.Th>
-              <Table.Th style={{ textAlign: 'right' }}>Rain Chance</Table.Th>
-              <Table.Th style={{ textAlign: 'right' }}>Temperature</Table.Th>
-            </Table.Tr>
-          </Table.Thead>
-          <Table.Tbody>{hourlyRows}</Table.Tbody>
-        </Table> 
-        :
-
-        <Center>
-          <Loader color="yellow" size="lg" mt={20}/>
-        </Center>
-        }
+        {hourlyRows ? (
+          <Table striped highlightOnHover>
+            <Table.Thead>
+              <Table.Tr>
+                <Table.Th>Time</Table.Th>
+                <Table.Th style={{ textAlign: 'right' }}>Rain Chance</Table.Th>
+                <Table.Th style={{ textAlign: 'right' }}>Temperature</Table.Th>
+              </Table.Tr>
+            </Table.Thead>
+            <Table.Tbody>{hourlyRows}</Table.Tbody>
+          </Table>
+        ) : (
+          <Center>
+            <Loader color="yellow" size="lg" mt={20} />
+          </Center>
+        )}
 
         {/* Table for the hourly forecast */}
       </AppShell.Main>
