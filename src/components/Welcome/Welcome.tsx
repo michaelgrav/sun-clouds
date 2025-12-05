f9e2db3ce2c3c191d2c3638f5279672739ed2c1b
Michael Graves <29133471+michaelgrav@users.noreply.github.com>
2025-12-04 21:56:09 -0500

    Format table better/change header text size on mobile

--- src/components/Welcome/Welcome.tsx
+++ src/components/Welcome/Welcome.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useRef, useState } from 'react';
 import axios from 'axios';
-import { AppShell, Burger, Center, Loader, Table, Title } from '@mantine/core';
-import { useDisclosure } from '@mantine/hooks';
+import { AppShell, Burger, Center, Loader, Skeleton, Table, Title, Text, Group } from '@mantine/core';
+import { useDisclosure, useMediaQuery } from '@mantine/hooks';
 
 export function Welcome() {
   const [opened, { toggle }] = useDisclosure();
@@ -24,6 +24,8 @@ export function Welcome() {
   }, []);
 
   const didFetchRef = useRef({ done: false });
+
+  const isSmall = useMediaQuery('(max-width: 768px)');
 
   useEffect(() => {
     if (latitude == null || longitude == null) {
@@ -82,6 +84,7 @@ export function Welcome() {
   const hourlyRows = hourlyWeatherForecast?.properties?.periods.map((period: any) => (
     <Table.Tr key={period.startTime}>
       <Table.Td>{formatHour(period.startTime)}</Table.Td>
+      <Table.Td>{period.probabilityOfPrecipitation.value + "%"}</Table.Td>
       <Table.Td>{period.temperature + period.temperatureUnit}</Table.Td>
     </Table.Tr>
   ));
@@ -97,32 +100,40 @@ export function Welcome() {
       }}
     >
       <AppShell.Header>
-        <Burger opened={opened} onClick={toggle} hiddenFrom="sm" size="sm" />
+        <Group style={{ width: '100%', display: 'flex', justifyContent: 'space-between', alignItems: 'center', height: '100%' }}>
+          <Group style={{ display: 'flex', alignItems: 'center', gap: 8 }}>
+            <Burger opened={opened} onClick={toggle} hiddenFrom="sm" size="sm" />
+            <Text size="lg" ml={10}>Sun Clouds</Text>
+          </Group>
 
-        <div>Sun Clouds</div>
-        <div>
-          {
-            weatherData ?
-            
-          "Forecast for " + weatherData?.properties?.relativeLocation?.properties?.city + ", " +
-          weatherData?.properties?.relativeLocation?.properties?.state
-          
-          :
-
-          ""
-          }
-        </div>
+          <Text
+            size={isSmall ? 'sm' : 'lg'}
+            ta="right"
+            mr={10}
+            style={{
+              textAlign: 'right',
+              maxWidth: isSmall ? '60%' : 400,
+              whiteSpace: 'nowrap',
+              overflow: 'hidden',
+              textOverflow: 'ellipsis',
+            }}
+          >
+            {weatherData
+              ? `Forecast for ${weatherData?.properties?.relativeLocation?.properties?.city}, ${weatherData?.properties?.relativeLocation?.properties?.state}`
+              : ''}
+          </Text>
+        </Group>
       </AppShell.Header>
 
       <AppShell.Navbar>
 
-        <Title order={5} ta="center" mt={50}>
+        <Title order={5} ta="center" mt={25} mb={15}>
           7-Day Forecast
         </Title>
 
         {/* return loading circle conditionally if the API call hasnt returned yet */}
         { dailyRows ?
-        <Table>
+        <Table striped highlightOnHover>
           <Table.Thead>
             <Table.Tr>
               <Table.Th>Day</Table.Th>
@@ -139,21 +150,24 @@ export function Welcome() {
       </AppShell.Navbar>
 
       <AppShell.Main>
-        <Title order={1} ta="center" mt={50}>
+        <Title order={1} ta="center" mt={5} mb={15}>
           Hourly Forecast
         </Title>
 
         {/* return loading circle conditionally if the API call hasnt returned yet */}
         { hourlyRows ? 
-        <Table>
+
+        <Table striped highlightOnHover>
           <Table.Thead>
             <Table.Tr>
               <Table.Th>Time</Table.Th>
+              <Table.Th>Rain Chance</Table.Th>
               <Table.Th>Temperature</Table.Th>
             </Table.Tr>
           </Table.Thead>
           <Table.Tbody>{hourlyRows}</Table.Tbody>
-        </Table> :
+        </Table> 
+        :
 
         <Center>
           <Loader color="yellow" size="lg" mt={20}/>
